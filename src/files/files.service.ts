e3987451db72fa33021352eb602c63286fd0c3ee
cdis001 <shopingid@naver.com>
2021-07-31 04:16:14 +0900

    fix. files service  - DB에 저장되는 이미지 URL 이름 변경  -> 바보같이 이전에 쓰던 createImageURL 함수를 지우지 않아서 엉뚱한 함수를 고쳐서 생긴 버그....

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -21,6 +21,6 @@ export class FilesService {
     createImageURL(file: Express.Multer.File): string {
         const serverAddress: string = process.env.SERVER_ADDRESS;
       
-        return `${serverAddress}/uploads/${file.originalname}`;
+        return `${serverAddress}/uploads/${file.filename}`;
       };
 }
