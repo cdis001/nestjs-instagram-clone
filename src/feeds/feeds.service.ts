a6e0b5a440434dc57b2b8537b33de8dbe98a6bae
cdis001 <shopingid@naver.com>
2021-08-18 20:56:15 +0900

    fix. feeds create  - feeds create에 try catch문 추가

--- src/feeds/feeds.service.ts
+++ src/feeds/feeds.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -63,6 +63,9 @@ export class FeedsService {
   }
 
   async create(feed: FeedsDTO) {
+    if (feed.files.length < 1) {
+      throw new HttpException('file is null', 401);
+    }
     const user = await this.userRepository.findOne({ id: feed.userId });
     const data = await this.feedRepository.create(feed);
     data.user = user;
